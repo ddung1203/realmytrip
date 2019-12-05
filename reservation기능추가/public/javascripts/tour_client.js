ffe7758d1bc5e9a247ce5f40833c77e5860d719e
jeonjungseok <jeonjungseok1203@gmail.com>
2019-12-05 12:15:29 +0900

    -기능수정

---
+++ reservation기능추가/public/javascripts/tour_client.js
@@ -0,0 +1,47 @@
+$(function() {
+  $('.tour-like-btn').click(function(e) {
+    var $el = $(e.currentTarget);
+    if ($el.hasClass('loading')) return;
+    $el.addClass('loading');
+    $.ajax({
+      url: '/api/tours/' + $el.data('id') + '/like',
+      method: 'POST',
+      dataType: 'json',
+      success: function(data) {
+        $('.tour .num-likes').text(data.numLikes);
+        $('.tour-like-btn').hide();
+      },
+      error: function(data, status) {
+        if (data.status == 401) {
+          alert('Login required!');
+          location = '/signin';
+        }
+        console.log(data, status);
+      },
+      complete: function(data) {
+        $el.removeClass('loading');
+      }
+    });
+  });
+
+  $('.question-like-btn').click(function(e) {
+    var $el = $(e.currentTarget);
+    if ($el.hasClass('disabled')) return;
+    $.ajax({
+      url: '/api/questions/' + $el.data('id') + '/like',
+      method: 'POST',
+      dataType: 'json',
+      success: function(data) {
+        $el.parents('.question').find('.num-likes').text(data.numLikes);
+        $el.addClass('disabled');
+      },
+      error: function(data, status) {
+        if (data.status == 401) {
+          alert('Login required!');
+          location = '/signin';
+        }
+        console.log(data, status);
+      }
+    });
+  });
+}); 
